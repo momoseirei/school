86ca2b5c54190336eca849700e45181f457581c6
森下晨弘 <momo@morishiBookpuro.elecom>
2020-03-23 19:09:29 +0900

    [ADD]: fizzbuzz

---
+++ js/fizzbuzz/main.js
@@ -0,0 +1,13 @@
+function fizzBuzz() {
+    for (let i=1; i<=100; i++) {
+        if (i%15 === 0){
+            console.log("FIZZ BUZZ");
+        } else if (i%3 === 0) {
+            console.log("FIZZ");
+        } else if (i%5 === 0) {
+            console.log("BUZZ");
+        } else {
+            console.log(i);
+        }
+    }
+};
