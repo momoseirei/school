e32775a21f8058779e413d73ffc6c84ec5068bd1
森下晨弘 <momo@morishiBookpuro.elecom>
2020-03-24 13:09:53 +0900

    [MOD]: 出力をコンソールからブラウザに変更

--- js/member_info/main.js
+++ js/member_info/main.js
@@ -1,46 +1,28 @@
+const tbody = document.getElementById("members-row");
+const members = [{
+    name: "山田太郎", age: 42, hobby: "ギター"
+}, {
+    name: "斎藤すぐる", age: 46, hobby: "車"
+}, {
+    name: "田中よしお", age: 32, hobby: "旅行、カメラ"
+}, {
+    name: "高橋とおる", age: 25, hobby: "絵を書くこと"
+}];
+
 function membersInfo() {
-    const members = [{
-        name: "山田太郎", age: 42, hobby: "ギター"
-    }, {
-        name: "斎藤すぐる", age: 46, hobby: "車"
-    }, {
-        name: "田中よしお", age: 32, hobby: "旅行、カメラ"
-    }, {
-        name: "高橋とおる", age: 25, hobby: "絵を書くこと"
-    }];
-
-    let age_20 = [];
-    let age_30 = [];
-    let age_40 = [];
-
+    tbody.innerHTML = "";
     members.forEach((member)=> {
-        if (20 <= member.age && member.age < 30) {
-            age_20.push(member);
-        } else if (30 <= member.age && member.age < 40) {
-            age_30.push(member);
-        } else if (40 <= member.age && member.age < 50) {
-            age_40.push(member);
-        } else {
-            console.log("出力対象外");
-        }
+        tbody.insertAdjacentHTML("afterbegin", "<tr><td>"+member.name+"</td><td>"+member.age+"</td><td>"+member.hobby+"</td></tr>");
     });
+}
 
-    console.log("-----20代-----");
-    age_20.forEach((member)=> {
-        console.log(member.name);
-        console.log(member.age + "歳");
-        console.log(member.hobby);
-    });
-    console.log("-----30代-----");
-    age_30.forEach((member)=> {
-        console.log(member.name);
-        console.log(member.age + "歳");
-        console.log(member.hobby);
-    });
-    console.log("-----40代-----");
-    age_40.forEach((member)=> {
-        console.log(member.name);
-        console.log(member.age + "歳");
-        console.log(member.hobby);
+function membersInfoSortAge(age) {
+    tbody.innerHTML = "";
+    members.forEach((member)=> {
+        if (age <= member.age && member.age < age+10) {
+            tbody.insertAdjacentHTML("afterbegin", "<tr><td>"+member.name+"</td><td>"+member.age+"</td><td>"+member.hobby+"</td></tr>");            
+        } else {
+            return;
+        }
     });
 }
